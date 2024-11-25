d02cff3279d58de5fa4f561498ad1edba71ae3a9
Miroslav Soukup <soukumi21@sps-prosek.cz>
2024-11-25 13:04:43 +0100

    small changes

---
+++ TEST_1_Miroslav_Soukup.c.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int a, b, c;
+    int discriminant;
+
+    // Debug: zaèátek programu
+    printf("Program zacal\n");
+
+    // Prompt the user for input
+    printf("Napis koeficient a: ");
+    if (scanf("%d", &a) != 1) {
+        printf("Error: Neplatny koeficient a\n");
+        exit(1);
+  #include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int a, b, c;
+    int diskriminant;
+
+    printf("Program zacal\n");
+
+    printf("Napis koeficient a: ");
+    if (scanf("%d", &a) != 1) {
+        printf("Error: Neplatny koeficient a\n");
+        exit(1);
+    }
+
+    printf("Napis koeficient b: ");
+    if (scanf("%d", &b) != 1) {
+        printf("Error: Neplatny koeficient b\n");
+        exit(1);
+    }
+
+    printf("Napis koeficient c: ");
+    if (scanf("%d", &c) != 1) {
+        printf("Error: Neplatny koeficient c\n");
+        exit(1);
+    }
+
+    printf("Koeficienty: a = %d, b = %d, c = %d\n", a, b, c);
+
+    diskriminant = b * b - 4 * a * c;
+
+    printf("Diskriminant: %d\n", diskriminant);
+
+    if (diskriminant < -999 || diskriminant > 999) {
+        printf("Error: Diskriminant mimo hranici\n");
+        exit(1);
+    }
+
+    printf("+-------+-------+-------+-------+\n");
+    printf("| %5d | %5d | %5d | %5d |\n", a, b, c, diskriminant);
+    printf("+-------+-------+-------+-------+\n");
+
+    if (diskriminant > 0) {
+        printf("|                Two real roots |\n");
+    } else if (diskriminant == 0) {
+        printf("|                      One root |\n");
+    } else {
+        printf("|               Imaginary roots |\n");
+    }
+
+    printf("+-------------------------------+\n");
+
+    return 0;
+}
--- student-packages/lecture-1/main.c
+++ student-packages/lecture-1/main.c
@@ -3,23 +3,37 @@
 
 #define LED_PIN 6
 #define BLINK_DELAY_MS 500
+#define BLINK_DELAY_MSM 1500
+
+
+gpio_init(LED_PIN);
+gpio_set_dir(LED_PIN, GPIO_OUT);
+
+
+void pomlcka()
+{
+    gpio_put(LED_PIN, 1);
+    sleep_ms(BLINK_DELAY_MSM);
+
+    // Turn off the LED
+    gpio_put(LED_PIN, 0);
+    sleep_ms(BLINK_DELAY_MSM);
+}
+
+void tecka()
+{
+    gpio_put(LED_PIN, 1);
+    sleep_ms(BLINK_DELAY_MS);
+
+    // Turn off the LED
+    gpio_put(LED_PIN, 0);
+    sleep_ms(BLINK_DELAY_MS);
+    
+}
 
 int main()
 {
-    // Initialize GPIO pins
-    gpio_init(LED_PIN);
-    gpio_set_dir(LED_PIN, GPIO_OUT);
-
-    // Infinite loop
-    while (1)
-    {
-        // Turn on the LED
-        gpio_put(LED_PIN, 1);
-        sleep_ms(BLINK_DELAY_MS);
-
-        // Turn off the LED
-        gpio_put(LED_PIN, 0);
-        sleep_ms(BLINK_DELAY_MS);
+    
     }
-    return 0;
-}
+  
+
